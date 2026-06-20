930ddb8128cc92c4c4cda7877fd72721329c8a47
2giddy <philrainey@email.com>
2026-06-21 09:39:38 +1000

    fixed errors

--- TickTackToe/Build/TickTackToe.loader.js
+++ TickTackToe/Build/TickTackToe.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 79018345,
+    wasmFileSize: 76787179,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
