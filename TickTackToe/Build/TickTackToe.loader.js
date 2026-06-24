233025ae73aa808a6fe1a12546994b25bef0d46f
2giddy <philrainey@email.com>
2026-06-24 21:55:13 +1000

    fixed mobile resizing

--- TickTackToe/Build/TickTackToe.loader.js
+++ TickTackToe/Build/TickTackToe.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 76789262,
+    wasmFileSize: 76789224,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
