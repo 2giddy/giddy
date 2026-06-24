b7fe51e03fa387eabeba058bf252f5d10747ee4b
2giddy <philrainey@email.com>
2026-06-24 19:53:28 +1000

    resize the board for phone

--- TickTackToe/Build/TickTackToe.loader.js
+++ TickTackToe/Build/TickTackToe.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 76787179,
+    wasmFileSize: 76789262,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
